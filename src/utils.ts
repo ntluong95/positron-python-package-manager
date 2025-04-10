623f6a4efabb4092852a0d0dc669707de0c512ab
ntluong95 <ph.ntluong95@gmail.com>
2025-04-10 13:33:18 +0200

    update comment for utils

--- src/utils.ts
+++ src/utils.ts
@@ -6,44 +6,45 @@ export function stripAnsi(text: string): string {
 
 export function getFilterRedundant(): boolean {
     const config = vscode.workspace.getConfiguration(
-        'positron-r-package-manager',
+        'positron-python-package-manager',
         vscode.Uri.file(vscode.workspace.workspaceFolders?.[0].uri.fsPath ?? "")
     );
     return config.get<boolean>('filterOutdatedIfUpToDateElsewhere', true);
 }
 
-// ✅ NEW: Move getImportName() here
 export function getImportName(packageName: string): string {
+    //TODO: WIP 
+    // This only list packages with package's name different with import name
+    // Python module import can be in my variation and unpredict
     const mappings: Record<string, string> = {
+        'beautifulsoup4': 'bs4',
+        'boto3': 'boto3',
+        'cryptography': 'cryptography',
+        'django': 'django',
+        'fastapi': 'fastapi',
+        'flask-sqlalchemy': 'flask_sqlalchemy',
+        'flask-wtf': 'flask_wtf',
+        'google-api-python-client': 'googleapiclient',
+        'google-cloud-storage': 'google.cloud.storage',
+        'ibis-framework': 'ibis',
+        'ipython': 'IPython',
+        'markupsafe': 'markupsafe',
         'opencv-python': 'cv2',
+        'paramiko': 'paramiko',
+        'psycopg2': 'psycopg2',
         'pillow': 'PIL',
-        'scikit-learn': 'sklearn',
+        'pydantic': 'pydantic',
+        'pyopenssl': 'OpenSSL',
+        'pymysql': 'pymysql',
+        'pytest': 'pytest',
         'pyyaml': 'yaml',
         'python-dateutil': 'dateutil',
         'python-dotenv': 'dotenv',
-        'beautifulsoup4': 'bs4',
-        'ibis-framework': 'ibis',
-        'flask-sqlalchemy': 'flask_sqlalchemy',
-        'flask-wtf': 'flask_wtf',
-        'pymysql': 'pymysql',
+        'scikit-learn': 'sklearn',
         'sqlalchemy': 'sqlalchemy',
-        'psycopg2': 'psycopg2',
-        'django': 'django',
-        'ujson': 'ujson',
-        'pyopenssl': 'OpenSSL',
-        'markupsafe': 'markupsafe',
-        'cryptography': 'cryptography',
-        'google-cloud-storage': 'google.cloud.storage',
-        'google-api-python-client': 'googleapiclient',
         'twilio': 'twilio',
-        'boto3': 'boto3',
-        'paramiko': 'paramiko',
-        'pytest': 'pytest',
-        'ipython': 'IPython',
-        'pydantic': 'pydantic',
-        'fastapi': 'fastapi'
+        'ujson': 'ujson'
     };
 
-    const lowerName = packageName.toLowerCase();
-    return mappings[lowerName] || lowerName.replace(/[-_.]/g, '');
+    return mappings[packageName] || packageName;
 }
